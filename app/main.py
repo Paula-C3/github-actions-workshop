0528907e648610836cdc3b8ca9c2e1d9259e1ff7
Paula <caicedo.paa@gmail.com>
2026-04-16 09:12:56 -0500

    add: deploy-prod en ci.yml

--- app/main.py
+++ app/main.py
@@ -1,22 +1,7 @@
-from fastapi import FastAPI         #type:ignore
-from pydantic import BaseModel      #type:ignore
-from app.calculator import sum, subtract, multiply
+from fastapi import FastAPI     #type: ignore
 
 app = FastAPI()
 
-class OperationInput(BaseModel):
-    a: int
-    b: int
-
-@app.post("/add")
-def add(data: OperationInput):
-    return {"result": sum(data.a, data.b)}
-
-@app.post("/subtract")
-def subtract_numbers(data: OperationInput):
-    return {"result": subtract(data.a, data.b)}
-
-@app.post("/multiply")
-def multiply_numbers(data: OperationInput):
-    return {"result": multiply(data.a, data.b)}
-
+@app.get("/")
+def dummy_get():
+    return {'message': "Hello from docker hub image and CI/CD workflow"}
